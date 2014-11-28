9a9f92ed7fb313cc046498a2fb6126fe3a13c8df
ruarrimactire <huracanster@gmail.com>
2014-11-28 16:30:07 +0100

    prueba0 --  no funciona -- como reallocar el espacio de memoria de un array de cadenas?

--- Ejercicios/III_control/ordena.c
+++ Ejercicios/III_control/ordena.c
@@ -2,22 +2,41 @@
  * File:   main.c
  * Author: Filippo
  *
- * Created on November 27, 2014, 1:30 PM
+ * Created on November 26, 2014, 1:39 PM
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAXLINES 100
 
-int PedirCadena(char **var) {
+char **lista;
+
+char **appendCadena(char **var2, int num, char *cad2){
+/*
+    //char *list[num];
+*/
+    char **list = (char **)malloc( num*sizeof(char *) );
+    int i;
+    
+    for(i = 0; i < num-1; i++){
+        list[i] = var2[i];
+    }
+    list[i] = (char *)malloc( strlen(cad2)+1 );
+    strcpy(list[i], cad2);
+/*
+    //free(var2);
+*/
+    lista = list;
+    return list;
+}
+
+int PedirCadena(char **var){
     char cad[90];
     int i = 0;
 
-    while( gets(cad)[0] != '\0' && i < MAXLINES ) {
-        var[i] = (char *)malloc(strlen(cad) + 1);
-        strcpy(var[i], cad);
+    while( gets(cad)[0] != '\0' ){
         i++;
+        appendCadena(var, i, cad);
     }
     return i;
 }
@@ -39,17 +58,22 @@ void OrdenarCadenas(char **var, int nfilas){
     }
 }
 
-int main() {
-    char *lista[MAXLINES];
+main() {
     int nfilas = 0;
     int i = 0;
 
-	printf("Introducir Lineas:\n");
     nfilas = PedirCadena(lista);
     
+/*
     if(lista == NULL){
         printf("Error");
         return 1;
+    }
+*/
+
+    printf("\nLista no ordenada de %d filas\n\n", nfilas);
+    for(i = 0; i < nfilas; i++){
+        printf("%s\n", lista[i]);
     }
 
     OrdenarCadenas(lista, nfilas);
@@ -58,6 +82,8 @@ int main() {
     for(i = 0; i < nfilas; i++){
         printf("%s\n", lista[i]);
     }
+
+    free(lista);
     
     return 0;          
 }
