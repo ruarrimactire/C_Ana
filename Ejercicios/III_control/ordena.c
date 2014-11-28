d504c14d4dfebb8a6b579d3331d69dc5538966a4
ruarrimactire <huracanster@gmail.com>
2014-11-28 16:34:56 +0100

    examen 3º ejercicio 2 bueno

--- Ejercicios/III_control/ordena.c
+++ Ejercicios/III_control/ordena.c
@@ -1,49 +1,68 @@
-/* 
+/*
  * File:   main.c
  * Author: Filippo
  *
- * Created on November 26, 2014, 6:30 PM
+ * Created on November 26, 2014, 3:50 PM
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#define MAXLINES 100
+#define MAXCHAR 90
 
-void PedirCadena(char **var, int nfilas){
-	char cad[90];
-	int i;
-	for(i = 0; i < nfilas; i++){
+int PedirCadena(char **var, int lineas) {
+    char cad[MAXCHAR];
+    int i = 0;
+
+	for(i = 0; i < lineas; i++){
 		gets(cad);
-		var[i]=(char *)malloc(strlen(cad)+1);
-		strcpy(var[i],cad);             
+		var[i] = (char *) malloc( strlen(cad)+1 ); //pongo +1 para reservar espacio por el \0
+        strcpy(var[i], cad);        
 	}
+    
+    return i;
 }
 
 void OrdenarCadenas(char **var, int nfilas){
-	char *aux;
-	int i = 0, s = 1;
-	while( (s == 1) && (--nfilas > 0) ){
-	}s=0;
-	for(i=1; i<=nfilas; i++){
-	}
-	if( strcmp(nombre[i-1], nombre[i]) > 0)
-	}
-	aux = nombre[i-1];
-	nombre[i-1] = nombre[i];
-	nombre[i] = aux;
-	s=1;
-	}
+    char *aux;
+    int i = 0, s = 1;
+    
+    while( (s == 1) && (--nfilas > 0) ){
+        s = 0;
+        for(i=1; i <= nfilas; i++){
+            if( strcmp(var[i-1], var[i]) > 0 ){
+                aux = var[i-1];
+                var[i-1] = var[i];
+                var[i] = aux;
+                s = 1; //S se convierte en 1 cuando if se cumple
+            }
+        } 
+    }
 }
 
-main() {
-	char **nombre==NULL;
-	int nfilas=100;
+int main() {
+    char **lista;
+    int nfilas = MAXLINES;
+    int i = 0;
 
-	nombre=(char **)malloc(nfilas*sizeof(char*));
-	if(nombre == NULL);
-	printf("Error");
-	PedirCadena(nombre, nfilas);
-	OrdenarCadenas(nombre, nfilas);
+    printf("Cuantas filas hay que leer?\n\n");
+    scanf("%d", &nfilas);
+    fflush(stdin);
+    
+    lista = (char **) malloc( nfilas*sizeof(char *) );
+    if(lista == NULL){
+        printf("Error");
+        return 1;
+    }
 
-	getchar();
-	 return 0;          
+    nfilas = PedirCadena(lista, nfilas);
+    OrdenarCadenas(lista, nfilas);
+
+    printf("\nLista ordenada de %d filas\n\n", nfilas);
+    for(i = 0; i < nfilas; i++){
+        printf("%s\n", lista[i]);
+    }
+
+    return 0;          
 }
